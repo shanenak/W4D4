adec8a0e737012f6a70c2061b86d2619c07dfb46
Shannon Millar <shanenak@gmail.com>
2023-08-10 14:50:44 -0700

    Finish #third_anagram?

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -3,5 +3,24 @@ def first_anagram?(str1, str2)
     anagrams.include?(str2.chars)
 end
 
-p first_anagram?("gizmo", "sally")    #=> false
-p first_anagram?("elvis", "lives")    #=> true
+# p first_anagram?("gizmo", "sally")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
+
+def second_anagram?(str1, str2)
+    str1.chars.each do |c|
+        i = str2.chars.find_index(c)
+        i.nil? ? false : str2.delete!(c)
+    end
+    str2.empty?
+end
+
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
+def third_anagram?(str1, str2)
+    str1.chars.sort == str2.chars.sort
+end
+
+p third_anagram?("gizmo", "sally")    #=> false
+p third_anagram?("elvis", "lives")    #=> true
+
