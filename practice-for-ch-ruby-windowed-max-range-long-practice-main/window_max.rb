679bb908462718b4d38eaf4f66b02ff359f74ebf
Shannon Millar <shanenak@gmail.com>
2023-08-10 17:09:18 -0700

    Finish StackQueue

--- practice-for-ch-ruby-windowed-max-range-long-practice-main/window_max.rb
+++ practice-for-ch-ruby-windowed-max-range-long-practice-main/window_max.rb
@@ -29,6 +29,10 @@ class MyQueue
         @store.length
     end
 
+    def empty?
+        @store.empty?
+    end
+
     def enqueue(ele)
         @store << ele
     end
@@ -36,6 +40,98 @@ class MyQueue
     def dequeue
         @store.shift
     end
-
-    
 end
+
+# q = MyQueue.new
+# q.enqueue('S')
+# q.enqueue('M')
+# p q.peek
+# p q.size
+# q.dequeue
+# p q.peek
+# q.dequeue
+# p q.size
+
+class MyStack
+  def initialize
+    @store = []
+  end
+
+  def push(ele)
+    @store << ele
+  end
+
+  def pop
+    @store.pop
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def peek
+    @store.last
+  end
+
+  def size
+    @store.size
+  end
+end
+
+# q = MyStack.new
+# q.push('S')
+# q.push('M')
+# p q.peek
+# p q.size
+# p q.empty?
+# q.pop
+# p q.peek
+# q.pop
+# p q.size
+# p q.empty?
+
+class StackQueue
+    def initialize
+        @staging = MyStack.new
+        @actual = MyStack.new
+    end
+
+    def size
+        @staging.size + @actual.size 
+    end
+
+    def empty?
+        @staging.empty? && @actual.empty?
+    end
+
+    def enqueue(ele)
+        while !@actual.empty?
+            @staging.push(@actual.pop)
+        end
+        @staging.push(ele)
+        while !@staging.empty?
+            @actual.push(@staging.pop)
+        end
+    end
+    
+    def dequeue
+        @actual.pop
+    end
+
+    def peek
+        @actual.peek
+    end
+
+end
+
+q = StackQueue.new
+q.enqueue('S')
+q.enqueue('M')
+p q.peek
+p q.size
+p q.empty?
+q.dequeue
+p q.peek
+q.dequeue
+p q.size
+p q.empty?
